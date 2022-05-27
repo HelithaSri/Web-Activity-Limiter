f4eb96b4feaa3926fba7b0844c6199e418c80947
HelithaSri <helitha.pravin@gmail.com>
2022-05-27 19:45:05 +0530

    Added jQuery and js file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const siteName = $("#siteName");
+const siteUrl = $("#siteUrl");
+const dailyLimit = $("#dLimit");
+// const form = $("#inputFormID");
+
+$("#addBtn").click(function(){
+    // console.log(siteName.val());
+    console.log($("#inputFormID").serialize());
+});
